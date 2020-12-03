26e27c7e3729c8a1019b4238182547330c6a368b
Chuck <chuck@intelligence.org>
2020-12-03 14:01:21 -0800

    Add license

--- default.nix
+++ default.nix
@@ -6,4 +6,9 @@ pkgs.python3Packages.callPackage
     version = "0.0.1";
     src = lib.cleanSource ./.;
     propagatedBuildInputs = [ jsonstreams more-itertools ];
+    meta = with lib; {
+      description = "A shell-friendly JSON encoder";
+      license = licenses.mit;
+      maintainers = with maintainers; [ chkno ];
+    };
   }) { }
