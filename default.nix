1c5f0cb1108854dfcdb6fb29f29141db84e28a48
Chuck <chuck@intelligence.org>
2020-12-05 12:00:50 -0800

    Start on 0.3.0

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ pkgs.python3Packages.callPackage
 ({ lib, buildPythonPackage, jsonstreams, more-itertools, }:
   buildPythonPackage rec {
     pname = "mkjson";
-    version = "0.2.0";
+    version = "0.3.0-pre";
     src = lib.cleanSource ./.;
     propagatedBuildInputs = [ jsonstreams more-itertools ];
     meta = with lib; {
